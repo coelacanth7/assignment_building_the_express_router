cc06cd9d1bc65ea923b99e19fe62a1e3ec9be358
Coelacanth7 <galford151@gmail.com>
2017-09-15 11:34:04 -0700

    router working"

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 //require gregs custom lib
 var gregsExpress = require("./lib/gregsexpress");
-var http = require("http");
 
 //store app function
 var app = gregsExpress();
+
+app.get("/", (req, res) => {
+	res.end("Oh shit, waddup");
+});
 
 var port = process.env.PORT || 3000;
 var host = "localhost";
 
 app.listen(port, host, () => {
-  console.log(`Listening at: http://${host}:${port}`);
+	console.log(`Listening at: http://${host}:${port}`);
 });
